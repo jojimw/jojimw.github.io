7c6fc68f10585d47f9e594be3d5279b046a52df2
Joji <jojipersonal@lm-kv-joji-3.local>
2023-01-22 04:35:03 +0530

    Updates

---
+++ component---src-pages-components-navbar-js-5cfdd2bfdabc37efb9b8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjojimw=self.webpackChunkjojimw||[]).push([[789],{9309:function(e,t,l){l.r(t);var a=l(7294),n=l(8037);t.default=function(){return a.createElement("header",null,a.createElement("nav",null,a.createElement(n.default,{to:"/",className:"logo"},a.createElement("h1",null,"J.")),a.createElement("ul",{className:"menu"},a.createElement("li",{className:"menu__list"},a.createElement(n.default,{to:"/contact/",className:"menu__list__link"},"Contact")))))}}}]);
+//# sourceMappingURL=component---src-pages-components-navbar-js-5cfdd2bfdabc37efb9b8.js.map
